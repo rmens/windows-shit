1132d68c7480527f924e0b7a8783d376abea8ef7
Raymon Mens <r.mens@me.com>
2019-07-08 00:06:16 +0200

    Update starthier.bat

--- starthier.bat
+++ starthier.bat
@@ -4,8 +4,8 @@ curl -o C:\Users\Public\time.bat https://raw.githubusercontent.com/rmens/windows
 curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/removew10apps.ps1 
 curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/windows-shit/master/remove%20onedrive%20w10.bat 
 curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/apps.exe 
-sudo.bat powershell.exe -file C:\Users\Public\removew10apps.ps1
-sudo.bat C:\Users\Public\power.bat
-sudo.bat C:\Users\Public\time.bat
-sudo.bat C:\Users\Public\removeod.bat
-sudo.bat C:\Users\Public\apps.exe
+CALL sudo.bat powershell.exe -file C:\Users\Public\removew10apps.ps1
+CALL sudo.bat C:\Users\Public\power.bat
+CALL sudo.bat C:\Users\Public\time.bat
+CALL sudo.bat C:\Users\Public\removeod.bat
+START sudo.bat C:\Users\Public\apps.exe
