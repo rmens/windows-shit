66e19239238c02e27e29f4e345ac917152ebc750
Raymon Mens <r.mens@me.com>
2019-07-07 23:24:45 +0200

    Autostart w32time

--- time.bat
+++ time.bat
@@ -6,3 +6,4 @@ net start w32time
 w32tm /config /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org" /syncfromflags:manual /reliable:no /update
 w32tm /config /update
 w32tm /resync /rediscover
+sc triggerinfo w32time delete
