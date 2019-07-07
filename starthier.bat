f81296e9694c1314f739fdb4ceaa57ee4369acc4
Raymon Mens <r.mens@me.com>
2019-07-07 23:20:03 +0200

    Kicker

---
+++ starthier.bat
@@ -0,0 +1,9 @@
+curl -o C:\Windows\sudo.bat https://raw.githubusercontent.com/zh0ul/sudo.bat/master/sudo.bat 
+curl -o C:\Users\Public\power.bat https://raw.githubusercontent.com/rmens/windows-shit/master/power.bat 
+curl -o C:\Users\Public\time.bat https://raw.githubusercontent.com/rmens/windows-shit/master/time.bat 
+curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/removew10apps.ps1 
+curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/windows-shit/master/remove%20onedrive%20w10.bat 
+sudo.bat powershell.exe -file C:\Users\Public\removew10apps.ps1
+sudo.bat C:\Users\Public\power.bat
+sudo.bat C:\Users\Public\time.bat
+sudo.bat C:\Users\Public\removeod.bat
