ec441b021e84a21f7699137c679f4a7df7e436a2
Raymon Mens <r.mens@me.com>
2019-07-08 00:26:52 +0200

    Add user creation

--- starthier.bat
+++ starthier.bat
@@ -3,9 +3,11 @@ curl -o C:\Users\Public\power.bat https://raw.githubusercontent.com/rmens/window
 curl -o C:\Users\Public\time.bat https://raw.githubusercontent.com/rmens/windows-shit/master/time.bat 
 curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/removew10apps.ps1 
 curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/windows-shit/master/remove_onedrive_w10.bat 
+curl -o C:\Users\Public\users.bat https://raw.githubusercontent.com/rmens/windows-shit/master/users.bat 
 curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/apps.exe 
 CALL sudo.bat powershell.exe -noexit -executionpolicy bypass -file C:\Users\Public\removew10apps.ps1
 CALL sudo.bat C:\Users\Public\power.bat
+CALL sudo.bat C:\Users\Public\users.bat
 CALL sudo.bat C:\Users\Public\time.bat
 CALL sudo.bat C:\Users\Public\removeod.bat
 START sudo.bat C:\Users\Public\apps.exe
