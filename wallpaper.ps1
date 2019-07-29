9eabc8d87723652657777c32999ae9b3a2d3706a
Raymon Mens <r.mens@me.com>
2019-07-29 12:52:10 -0700

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,2 +1,7 @@
-. C:\Users\Public\Set-Wallpaper.ps1
-Set-Wallpaper -All -WallPaperFile C:\Users\Public\wallpaper.bmp
+ foreach($UserHive in Get-ChildItem Registry::HKEY_USERS){
+            $DesktopKeyPath = (Join-Path $UserHive.PSPath "Control Panel\Desktop")
+            if(Test-Path $DesktopKeyPath){
+                Set-ItemProperty -Path $DesktopKeyPath -Name Wallpaper -Value C:\users\Public\wallpaper.bmp
+            }
+             rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
