59b4feca827aea284dad482892d2d42b43f2ff19
Raymon Mens <r.mens@me.com>
2019-07-29 21:15:23 +0200

    Fix wallpaper

--- starthier.bat
+++ starthier.bat
@@ -6,10 +6,10 @@ curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/win
 curl -o C:\Users\Public\users.bat https://raw.githubusercontent.com/rmens/windows-shit/master/users.bat 
 curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/apps.exe 
 curl -o C:\Users\Public\wallpaper.bmp http://paulbourke.net/dataformats/bmp/sample.bmp
-curl -o C:\Users\Public\Set-Wallpaper.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/Set-Wallpaper.ps1
+curl -o C:\Users\Public\wallpaper.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/wallpaper.ps1
 curl -o C:\Users\Public\Set-Wallpaper.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/Set-Wallpaper.ps1
 CALL sudo.bat powershell.exe -noexit -executionpolicy bypass -file C:\Users\Public\removew10apps.ps1
-CALL sudo.bat powershell.exe -noexit -executionpolicy bypass -file C:\Users\Public\wallpaper.ps1
+CALL sudo.bat powershell.exe -noexit -executionpolicy bypass -file C:\Users\Public\Set-Wallpaper.ps1
 CALL sudo.bat C:\Users\Public\power.bat
 CALL sudo.bat C:\Users\Public\users.bat
 CALL sudo.bat C:\Users\Public\time.bat
