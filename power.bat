3a1f6dd059d01f890aa2ff31f7431df14a452d07
Raymon Mens <r.mens@me.com>
2019-07-30 14:24:20 +0200

    Update power.bat

--- power.bat
+++ power.bat
@@ -1,5 +1,5 @@
-powercfg /change monitor-timeout-ac 0
-powercfg /change monitor-timeout-dc 0
+powercfg /change monitor-timeout-ac 30
+powercfg /change monitor-timeout-dc 30
 powercfg /change disk-timeout-ac 0
 powercfg /change disk-timeout-dc 0
 powercfg /change standby-timeout-ac 0
