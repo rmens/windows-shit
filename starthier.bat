2250ad7e3f68d9a545a02064cfafb83f5b663389
rmens <r.mens@me.com>
2019-07-30 23:51:29 +0200

    Userpol

--- starthier.bat
+++ starthier.bat
@@ -5,7 +5,9 @@ curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmen
 curl -o C:\Users\Public\workgroup.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/workgroup.ps1 
 curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/remove_onedrive_w10.bat 
 curl -o C:\Users\Public\users.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/users.bat 
-curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/assets/apps.exe 
+curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/bin/apps.exe 
+curl -o C:\Users\Public\LGPO.exe https://raw.githubusercontent.com/rmens/windows-shit/master/bin/LGPO.exe 
+curl -o C:\Users\Public\userpol.pol https://raw.githubusercontent.com/rmens/windows-shit/master/assets/userpol.pol 
 curl -o C:\Users\Public\wallpaper.bmp http://paulbourke.net/dataformats/bmp/sample.bmp
 curl -o C:\Windows\PolicyDefenitions\7-Zip.admx https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.admx
 curl -o C:\Windows\PolicyDefenitions\en-US\7-Zip.adml https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.adml
@@ -16,3 +18,4 @@ CALL sudo.bat C:\Users\Public\users.bat
 CALL sudo.bat C:\Users\Public\time.bat
 CALL sudo.bat C:\Users\Public\removeod.bat
 START C:\Users\Public\apps.exe
+START C:\Users\Public\LGPO.exe /un userpol.pol
