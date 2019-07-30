455b5b633e6eea9b3ca2ce2a64f1092d9bdcc9de
Raymon Mens <r.mens@me.com>
2019-07-30 13:57:41 +0200

    Update users.bat

--- users.bat
+++ users.bat
@@ -1,3 +1,4 @@
 net user "Studio Gebruiker" /ADD
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "Studio Gebruiker" /f
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "" /f
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
