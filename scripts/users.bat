3e9ebbde5f3d287b065145edce056545985e118a
rmens <r.mens@me.com>
2019-07-31 00:24:34 +0200

    small fixes

--- scripts/users.bat
+++ scripts/users.bat
@@ -1,4 +1,4 @@
-net user "Studio Gebruiker" /ADD
+net user "Studio Gebruiker" oszw /ADD
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "Studio Gebruiker" /f
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "oszw" /f
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
