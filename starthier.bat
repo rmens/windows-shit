fe5a0c987d115f404590dab9c90c9d23f9734219
rmens <r.mens@me.com>
2019-07-30 18:00:51 +0200

    Beginnetje met GPO voor user

--- starthier.bat
+++ starthier.bat
@@ -1,4 +1,4 @@
-curl -o C:\Windows\sudo.bat https://raw.githubusercontent.com/zh0ul/sudo.bat/master/sudo.bat 
+curl -o C:\Windows\sudo.bat https://raw.githubusercontent.com/zh0ul/sudo.bat/master/sudo.bat
 curl -o C:\Users\Public\power.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/power.bat 
 curl -o C:\Users\Public\time.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/time.bat 
 curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/removew10apps.ps1 
@@ -6,6 +6,8 @@ curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/win
 curl -o C:\Users\Public\users.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/users.bat 
 curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/assets/apps.exe 
 curl -o C:\Users\Public\wallpaper.bmp http://paulbourke.net/dataformats/bmp/sample.bmp
+curl -o C:\Windows\PolicyDefenitions\7-Zip.admx https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.admx
+curl -o C:\Windows\PolicyDefenitions\en-US\7-Zip.adml https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.adml
 CALL sudo.bat powershell.exe -executionpolicy bypass -file "C:\Users\Public\removew10apps.ps1"
 CALL sudo.bat C:\Users\Public\power.bat
 CALL sudo.bat C:\Users\Public\users.bat
