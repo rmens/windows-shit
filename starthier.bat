a347e8592f02c1e270c223f436c7c7bcd212a19e
rmens <r.mens@me.com>
2019-07-30 23:36:11 +0200

    add to workgroup

--- starthier.bat
+++ starthier.bat
@@ -2,6 +2,7 @@ curl -o C:\Windows\sudo.bat https://raw.githubusercontent.com/zh0ul/sudo.bat/mas
 curl -o C:\Users\Public\power.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/power.bat 
 curl -o C:\Users\Public\time.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/time.bat 
 curl -o C:\Users\Public\removew10apps.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/removew10apps.ps1 
+curl -o C:\Users\Public\workgroup.ps1 https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/workgroup.ps1 
 curl -o C:\Users\Public\removeod.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/remove_onedrive_w10.bat 
 curl -o C:\Users\Public\users.bat https://raw.githubusercontent.com/rmens/windows-shit/master/scripts/users.bat 
 curl -o C:\Users\Public\apps.exe https://raw.githubusercontent.com/rmens/windows-shit/master/assets/apps.exe 
@@ -9,6 +10,7 @@ curl -o C:\Users\Public\wallpaper.bmp http://paulbourke.net/dataformats/bmp/samp
 curl -o C:\Windows\PolicyDefenitions\7-Zip.admx https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.admx
 curl -o C:\Windows\PolicyDefenitions\en-US\7-Zip.adml https://raw.githubusercontent.com/rmens/windows-shit/master/assets/7-Zip.adml
 CALL sudo.bat powershell.exe -executionpolicy bypass -file "C:\Users\Public\removew10apps.ps1"
+CALL sudo.bat powershell.exe -executionpolicy bypass -file "C:\Users\Public\workgroup.ps1"
 CALL sudo.bat C:\Users\Public\power.bat
 CALL sudo.bat C:\Users\Public\users.bat
 CALL sudo.bat C:\Users\Public\time.bat
