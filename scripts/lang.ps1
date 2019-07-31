53f2a1abca7517d656fec6e5f20b0443d284e839
rmens <r.mens@me.com>
2019-07-31 17:13:26 +0200

    Update lang.ps1

--- scripts/lang.ps1
+++ scripts/lang.ps1
@@ -1,4 +1,5 @@
-Set-WinSystemLocale nl-NL -Force
+Set-WinSystemLocale nl-NL
 Set-WinUserLanguageList -Language nl-NL -Force
 Set-TimeZone -Name "W. Europe Standard Time"
-Set-Culture -CultureInfo nl-NL
+Set-Culture -CultureInfo nl-NL NoUserOverrides
+Set-WinHomeLocation -GeoId 0xb0
