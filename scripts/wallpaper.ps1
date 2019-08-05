57e01f1fdb3a8814795f209db00a6efafd80735c
rmens <r.mens@me.com>
2019-08-05 21:57:02 +0200

    Delete wallpaper.ps1

--- scripts/wallpaper.ps1
+++
@@ -1,7 +0,0 @@
- foreach($UserHive in Get-ChildItem Registry::HKEY_USERS){
-            $DesktopKeyPath = (Join-Path $UserHive.PSPath "Control Panel\Desktop")
-            if(Test-Path $DesktopKeyPath){
-                Set-ItemProperty -Path $DesktopKeyPath -Name Wallpaper -Value C:\users\Public\wallpaper.bmp
-            }
-             rundll32.exe user32.dll, UpdatePerUserSystemParameters
-}
