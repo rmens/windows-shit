facaec560b24dd428786eede37817fb3de9c3fe2
Raymon Mens <r.mens@me.com>
2019-08-11 17:51:23 +0200

    Update removew10apps.ps1

--- scripts/removew10apps.ps1
+++ scripts/removew10apps.ps1
@@ -44,12 +44,13 @@ Get-AppxPackage -AllUsers Microsoft.Messaging | Remove-AppxPackage -AllUsers
 Get-AppXProvisionedPackage -Online | Remove-AppxProvisionedPackage -Online -AllUsers
 
 # SMBv1 wegens veiligheid
-Disable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol"
+Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName "SMB1Protocol"
 
 # We hoeven niets meer met IE11
-Disable-WindowsOptionalFeature -Online -FeatureName "Interet-Explorer-Optional-amd64"
+Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName "Interet-Explorer-Optional-amd64"
 
 # XPS printer is niet echt nuttig
-Disable-WindowsOptionalFeature -Online -FeatureName "Printing-XPSServices-Features"
+Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName "Printing-XPSServices-Features"
+
 # Lijstje maken van geinstalleerde apps
 #Get-AppxPackage -AllUsers | Select Name, PackageFullName
