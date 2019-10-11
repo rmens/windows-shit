07362b5d694d52999b3168fef393e3d2f5417ef9
Raymon Mens <r.mens@me.com>
2019-10-11 16:21:15 +0200

    Make sure local passwords never expire

--- scripts/users.bat
+++ scripts/users.bat
@@ -3,3 +3,4 @@ REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultU
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "oszw" /f
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "DisableEdgeDesktopShortcutCreation" /t REG_DWORD /d "1" /f
+net accounts /maxpwage:unlimited
