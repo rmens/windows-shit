89a1c5b92180b427bed56c2f11a98a4842b03465
Raymon Mens <r.mens@me.com>
2019-12-07 17:59:42 +0100

    Update filer.bat

--- scripts/filer.bat
+++ scripts/filer.bat
@@ -1,3 +1,4 @@
 @echo off
 net use f: \\10.1.2.12\filer /USER:file-storage\zwtv zuidwest21 /PERSISTENT:no
-net use i: \\10.10.80.21\import /USER:"PLAYOUT-BOZ\Studio Gebruiker" oszw /PERSISTENT:no
+net use i: \\10.1.1.92\import /USER:"PLAYOUT-BOZ\Studio Gebruiker" oszw /PERSISTENT:no
+net use b: \\10.1.2.11\backup_nas /PERSISTENT:no
