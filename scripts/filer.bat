d83928b2169f295eaaad6dbdb372b2d7491328d8
Raymon Mens <r.mens@me.com>
2019-12-08 20:49:02 +0100

    Update filer.bat

--- scripts/filer.bat
+++ scripts/filer.bat
@@ -4,3 +4,4 @@ net use i: \\10.1.1.92\import /USER:"PLAYOUT-BOZ\Studio Gebruiker" oszw /PERSIST
 :: net use b: \\10.1.2.11\backup_nas /PERSISTENT:no
 :: net use w: "\\edit-03\D-schijf Edit 03" /PERSISTENT:no
 :: net use u: "\\edit-02\D-schijf Edit 02" /PERSISTENT:no
+:: net use s: "\\edit-01\D-schijf Edit 01" /PERSISTENT:no
