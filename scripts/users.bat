93f246129b2d2e34e4e0e760d918167979c5416f
Raymon Mens <r.mens@me.com>
2021-05-11 22:22:53 +0200

    Add AutoAdminLogon

--- scripts/users.bat
+++ scripts/users.bat
@@ -3,6 +3,6 @@ net user "Edit Gebruiker" oszw /ADD
 net user "Studio Gebruiker" oszw /ADD
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "Redactie Gebruiker" /f
 REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "oszw" /f
-REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "DisableEdgeDesktopShortcutCreation" /t REG_DWORD /d "1" /f
 net accounts /maxpwage:unlimited
