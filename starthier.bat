f0364a15cdefb3e6fb61311748c2aa206d16f54b
Raymon Mens <r.mens@me.com>
2021-05-11 18:03:40 +0200

    Add debloat script on run

--- starthier.bat
+++ starthier.bat
@@ -21,6 +21,7 @@ curl -o C:\Windows\deploy\ZWFM-wallpaper.png https://raw.githubusercontent.com/r
 curl -o C:\Windows\deploy\ZWTV-wallpaper.png https://raw.githubusercontent.com/rmens/windows-shit/master/assets/ZWTV-wallpaper.png
 curl -o C:\Windows\deploy\ZWUP-wallpaper.png https://raw.githubusercontent.com/rmens/windows-shit/master/assets/ZWUP-wallpaper.png
 curl -o C:\Windows\deploy\tv.reg https://raw.githubusercontent.com/rmens/windows-shit/master/assets/tv.reg
+curl -o C:\Windows\deploy\debloat.reg https://raw.githubusercontent.com/rmens/windows-shit/master/assets/debloat.reg
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\userpol.txt /w C:\Windows\deploy\userpol.pol
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\syspol.txt /w C:\Windows\deploy\syspol.pol
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\firefoxpol.txt /w C:\Windows\deploy\firefoxpol.pol
@@ -33,4 +34,5 @@ CALL sudo.bat powershell.exe -executionpolicy bypass -file "C:\Windows\deploy\re
 START C:\Windows\deploy\LGPO.exe /un C:\Windows\deploy\userpol.pol
 START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\syspol.pol
 START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\firefoxpol.pol
+CALL sudo.bat REG import C:\windows\deploy\debloat.reg
 CALL sudo.bat C:\Windows\deploy\apps.bat
